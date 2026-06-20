b459770dfb5e205e9fd2ef15aa26be5cb349b1d5
T0mmy-3 <141057232+T0mmy-3@users.noreply.github.com>
2026-06-21 00:32:55 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-// Minimal configuration to use pre-installed packages only
 export default defineConfig({
   plugins: [
+    react(),
     tsconfigPaths()
   ],
   resolve: {
